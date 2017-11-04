ff38f2567e47639f40a40859b83019ec17f97e59
Michael Beckwith <tw2113@gmail.com>
2017-11-03 23:02:43 -0500

    add support for checking for basic singular views, eventually shortcodes, and widgets

--- collectors/meta.php
+++ collectors/meta.php
@@ -35,6 +35,52 @@ class QueryMonitor_GiveWP_Collector_Meta extends \QM_Collector {
 			}
 		}
 	}
+
+	private function has_give_form( \WP_Post $post ) {
+		if ( is_admin() ) {
+			return;
+		}
+		if ( 'give_forms' === $post->post_type ) {
+			if ( ! is_singular() ) {
+				return;
+			}
+			$this->give_has_form = true;
+			$this->give_post_id  = $post->ID;
+
+			return;
+		}
+
+		// @todo Figure out why parsing isn't returning attributes correctly.
+		$all_shortcode_atts = shortcode_parse_atts( $post->post_content );
+		if ( ! empty( $all_shortcode_atts['id'] ) ) {
+			$maybe_give_form = get_post( absint( $all_shortcode_atts['id'] ) );
+			if ( $maybe_give_form instanceof \WP_Post && 'give_forms' === $maybe_give_form->post_type ) {
+				$this->give_has_form = true;
+				$this->give_post_id  = $maybe_give_form->ID;
+
+				return;
+			}
+		}
+
+		$has_widget = is_active_widget( false, false, 'give_forms_widget' );
+		if ( false !== $has_widget ) {
+			// Grab all of our sidebar areas.
+			$sidebars_widgets    = wp_get_sidebars_widgets();
+			$widget_id           = $sidebars_widgets[ $has_widget ][0]; // Grab the widget ID that we want to run with.
+			$active_give_widgets = get_option( 'widget_give_forms_widget' );
+			$widget_numeral_id   = array_pop( explode( '-', $widget_id ) ); // Pluck the ID of the widget off the end.
+			$desired_widget_data = $active_give_widgets[ $widget_numeral_id ]; // Grab the specific instance out of our array of saved widgets.
+			if ( ! empty( $desired_widget_data['id'] ) ) {
+				$maybe_widget_give_form = get_post( absint( $desired_widget_data['id'] ) );
+				if ( $maybe_widget_give_form instanceof \WP_Post && 'give_forms' === $maybe_widget_give_form->post_type ) {
+					$this->give_has_form = true;
+					$this->give_post_id  = $maybe_widget_give_form->ID;
+
+					return;
+				}
+			}
+		}
+	}
 }
 
 function register_qm_gwp_collectors_meta( array $collectors, \QueryMonitor $qm ) {
