172d1682a9567ab57c50f16fb454334c4a31493f
Michael Beckwith <tw2113@gmail.com>
2017-11-07 20:05:05 -0600

    finish current meta output phpdocs

--- outputters/meta.php
+++ outputters/meta.php
@@ -21,6 +21,11 @@ class QueryMonitor_GiveWP_HTML_Meta extends \QM_Output_Html {
 		add_filter( 'qm/output/menus', array( $this, 'admin_menu' ), 101 );
 	}
 
+	/**
+	 * Construct the output for the Query Monitor content section for GiveWP meta.
+	 *
+	 * @since 1.0.0
+	 */
 	public function output() {
 		$data = $this->collector->get_data();
 
@@ -65,6 +70,14 @@ class QueryMonitor_GiveWP_HTML_Meta extends \QM_Output_Html {
 		<?php
 	}
 
+	/**
+	 * Add our meta section to the Query Monitor dropdown from admin bar.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $menu Array of menu items to render.
+	 * @return array
+	 */
 	public function admin_menu( array $menu ) {
 		$add = array(
 			'title' => sprintf( esc_html__( '%s Meta', 'query-monitor-givewp' ), 'GiveWP' ),
@@ -76,6 +89,16 @@ class QueryMonitor_GiveWP_HTML_Meta extends \QM_Output_Html {
 	}
 }
 
+/**
+ * Initiate an instance for HTML output for the meta section.
+ *
+ * @since 1.0.0
+ *
+ * @param array          $output     Array of HTML output instances to render.
+ * @param \QM_Collectors $collectors Collector object.
+ *
+ * @return array
+ */
 function register_qm_gwp_output_html_meta( array $output, \QM_Collectors $collectors ) {
 	if ( $collector = \QM_Collectors::get( 'qmgwp-meta' ) ) {
 		$output['qmgwp-meta'] = new QueryMonitor_GiveWP_HTML_Meta( $collector );
